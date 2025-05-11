009534999b28f2ced983f379d129831a8082003d
Thakuei <c1247183@st.kanazawa-it.ac.jp>
2025-05-11 20:22:43 +0900

    jobの追加

--- backend/app/jobs/study_record_completion_job.rb
+++ backend/app/jobs/study_record_completion_job.rb
@@ -4,33 +4,32 @@ class StudyRecordCompletionJob < ApplicationJob
   def perform(slack_user_id, response_url)
     user = User.find_by(slack_id: slack_user_id)
     unless user
-      post_to_slack(response_url, "ユーザーが登録されていません。")
+      notify_slack(response_url, "ユーザーが登録されていません。")
       return
     end
 
     study_record = user.study_records.order(start_time: :desc).first
     unless study_record
-      post_to_slack(response_url, "勉強開始記録が見つかりません。")
+      notify_slack(response_url, "勉強開始記録が見つかりません。")
       return
     end
 
     if study_record.end_time
-      post_to_slack(response_url, "この勉強記録はすでに終了しています。")
+      notify_slack(response_url, "この勉強記録はすでに終了しています。")
       return
     end
 
     study_record.update!(end_time: Time.current)
     study_time = ((study_record.end_time - study_record.start_time) / 60).round(2)
 
-    post_to_slack(response_url, "<@#{slack_user_id}>さんが勉強を終了しました！所要時間 #{study_time}分。")
+    notify_slack(response_url, "<@#{slack_user_id}>さんが勉強を終了しました！所要時間 #{study_time}分。")
   rescue => e
-    post_to_slack(response_url, "エラーが発生しました: #{e.message}")
+    notify_slack(response_url, "エラーが発生しました: #{e.message}")
   end
 
   private
 
-  def post_to_slack(response_url, message)
-    uri = URI.parse(response_url)
-    Net::HTTP.post(uri, { text: message }.to_json, "Content-Type" => "application/json")
+  def notify_slack(response_url, message)
+    Slack::Notifier.new(response_url).ping(message)
   end
 end
--- backend/app/jobs/study_record_creation_job.rb
+++ backend/app/jobs/study_record_creation_job.rb
@@ -8,7 +8,6 @@ class StudyRecordCreationJob < ApplicationJob
 
     StudyRecord.create!(user_id: user.id, start_time: Time.current)
 
-    post_to_slack(response_url, "<@#{slack_user_id}>さんの勉強記録を開始しました！")
   rescue => e
     post_to_slack(response_url, "エラーが発生しました: #{e.message}")
   end
@@ -16,7 +15,7 @@ class StudyRecordCreationJob < ApplicationJob
   private
 
   def post_to_slack(response_url, message)
-    uri = URI.parse(response_url)
-    Net::HTTP.post(uri, { text: message }.to_json, "Content-Type" => "application/json")
+    notifier = Slack::Notifier.new(response_url)
+    notifier.ping(message)
   end
 end
