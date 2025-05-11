c055ca2973f8657e74b3a824d12649dd13a17694
Thakuei <c1247183@st.kanazawa-it.ac.jp>
2025-05-11 21:34:45 +0900

    fix:2重送信を防ぐ

--- backend/app/controllers/api/v1/study_records_controller.rb
+++ backend/app/controllers/api/v1/study_records_controller.rb
@@ -2,21 +2,13 @@ module Api::V1
   class Api::V1::StudyRecordsController < ApplicationController
     # skip_before_action :verify_authenticity_token
     def start
-      render json: { 
-        response_type: "in_channel",
-        text: "<@#{params[:user_id]}>さんが勉強を開始しました！" 
-      }
-
       StudyRecordCreationJob.perform_later(params[:user_id], params[:user_name])
+      head :ok
     end
 
     def end
-      render json: { 
-        response_type: "in_channel",
-        text: "<@#{params[:user_id]}>さんが勉強を終了しました!" 
-      }, status: :ok
-
       StudyRecordCompletionJob.perform_later(params[:user_id])
+      head :ok
     end
   end
 end
