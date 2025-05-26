da87430db7d266e57a47b1f5b627617e248e50e5
Thakuei <c1247183@st.kanazawa-it.ac.jp>
2025-05-26 16:01:42 +0900

    違反修正

--- backend/app/models/study_record.rb
+++ backend/app/models/study_record.rb
@@ -4,7 +4,7 @@ class StudyRecord < ApplicationRecord
   def self.force_finish_all
     where(end_time: nil).find_each do |record|
       record.update(end_time: Time.current)
-      
+
       Slack::Notifier.new(ENV["SLACK_WEBHOOK_URL"]).ping(
         "<@#{record.user.slack_id}>さんの勉強を21時になったので強制終了しました！"
       )
